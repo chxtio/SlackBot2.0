af4505b38eb2c7d0b8a45f02c5495d6b16599438
chxtio <chxtio@gmail.com>
2020-12-26 22:25:53 -0800

    Added onboarding script and App home

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SlackBot2.0
+
+![image](https://user-images.githubusercontent.com/33184844/103165105-95f7ac00-47c8-11eb-91c1-56cfa959f432.png)
+
+A simple bot created with [Slack Bolt for Python](https://api.slack.com/start/building/bolt-python) using [Slack's Event API](https://api.slack.com/events).
+The app will greet new users and create an interactive modal for introductions. A basic App home was also added, with features
+such as displaying tips whenever the user clicks into the App home. Tips can be generated via a slash command as well.
+
+
+###### Installation:
+	CD into the folder
+	ngrok http 3000
+	In a separate CMD shell, CD into the folder
+	python -m venv .venv
+	./.venv/Scripts/activate
+	set SLACK_BOT_TOKEN=
+	set SLACK_SIGNING_SECRET=
+	pip install slack_bolt
+	python appStart.py
+
+![image](https://user-images.githubusercontent.com/33184844/103165160-fb4b9d00-47c8-11eb-816f-09f7ab70eca0.png)
--- appStart.py
+++ appStart.py
@@ -46,7 +46,57 @@ def app_mentioned(body, event, logger, say, client):
     say(f"Hello there <@{event['user']}>")
     client.chat_postEphemeral(channel=event['channel'], user=event['user'], text="Feel free to ask me anything!")
 
-@app.command("/sb2.0_tips")
+@app.event("member_joined_channel")
+def event_join(ack, body, say, logger, client):
+    ack()
+    logger.info("Testing member_joined_channel event\n")
+    logger.info(body)
+
+    if body['event']['channel'] == 'C01HFBP03GE':
+        text = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hello <@{body['event']['user']}>,\n would you like to introduce yourself a bit? "
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://api.slack.com/img/blocks/bkb_template_images/approvalsNewDevice.png",
+                    "alt_text": "computer thumbnail"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "YES"
+                        },
+                        "action_id": "intro_yes_button",
+                        "style": "primary",
+                        "value": "yes"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "NO"
+                        },
+                        "action_id": "intro_nope_button",
+                        "style": "danger",
+                        "value": "nope"
+                    }
+                ]
+            }
+        ]
+        client.chat_postMessage(
+            channel=body['event']['channel'], user=body['event']['user'], blocks=text)
+
+
+@app.command("/slackbot2.0_tips")
 def tips_called(ack, body, command, logger, client):
     ack()
     logger.info("Testing slackbot2.0_tips")
@@ -99,6 +149,217 @@ def tip_button_clicked(body, action, ack, logger):
     ack()
     logger.info("Testing tip_button_clicked")
     logger.info(action)
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        tip = random.choice(tips)
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+            # the user that opened your app's app home
+            user_id = event["user"],
+            # the view object that appears in the app home
+            view = {
+                "type": "home",
+                "callback_id": "home_view",
+
+                # body of the view
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to your _App's Home_* :tada:"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "`Tip:` " + tip
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Show more tips",
+                                    "emoji": True
+                                },
+                                "value": "click_me_123",
+                                "action_id": "show_more_tips"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "action_id": "HelloThere",
+                                "type": "button",
+                                "style": "primary",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Click me now!"
+                                }
+                            },
+                        ]
+                    }
+                ]
+
+
+            }
+        )
+
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+# Example button for button on App home which just posts a message to the user in the
+# apps user channel
+@app.action("HelloThere")
+def react_to_button(body, logger, client):
+    logger.info(body)
+    client.chat_postMessage(
+        channel=body["user"]["id"], text="You pushed a button!")
+
+@app.action("intro_yes_button")
+def yes_button(body, action, logger, client, ack):
+    ack()
+
+    ## triggering a modal view
+    res = client.views_open(
+        trigger_id=body["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "intro_modal",
+            "title": {"type": "plain_text", "text": "My Intro",},
+            "submit": {"type": "plain_text", "text": "Submit",}, # has a submit button is optional, that submit will trigger callback_id
+            "close": {"type": "plain_text", "text": "Cancel",},
+            "private_metadata": f"{body['channel']['id']},{body['message']['ts']}", ## stored so we can update the original message
+            "blocks": [ ## create 4 input fields
+                {
+                    "type": "input",
+                    "block_id":"where",
+                    "element": {"type": "plain_text_input", "action_id": "where"},
+                    "label": {"type": "plain_text", "text": "Where are you from?",},
+                },
+                {
+                    "type": "input",
+                    "block_id":"you",
+                    "element": {"type": "plain_text_input", "action_id": "you"},
+                    "label": {"type": "plain_text", "text": "Tell us about yourself",},
+                },
+                {
+                    "type": "input",
+                    "block_id":"learn",
+                    "element": {"type": "plain_text_input", "action_id": "learn"},
+                    "label": {"type": "plain_text", "text": "What do you want to learn",},
+                },
+                {
+                    "type": "input",
+                    "block_id":"project",
+                    "element": {"type": "plain_text_input", "action_id": "project"},
+                    "label": {"type": "plain_text", "text": "What project would you like to work on?",},
+                }
+            ],
+        },
+    )
+
+
+    logger.info(res)
+
+### reads out our simple modal and formats the result a bit
+@app.view('intro_modal')
+def intro_modal_submitted(body, ack, logger, client):
+	ack()
+	logger.info("Modal Submitted")
+
+	## reading the data from the input fields
+	you = body['view']['state']['values']['you']['you']['value']
+	learn = body['view']['state']['values']['learn']['learn']['value']
+	where = body['view']['state']['values']['where']['where']['value']
+	project = body['view']['state']['values']['project']['project']['value']
+
+	# reading metadata from the original channel and ts that was added
+	data = body['view']['private_metadata'].split(',')
+	block = [
+		        {
+		            "type": "section",
+		            "text": {
+		                "type": "mrkdwn",
+		                "text": f"<@{body['user']['id']}> INTRO"
+		            }
+		        },
+		        {
+		            "type": "section",
+		            "text": {
+		                "type": "mrkdwn",
+		                "text": f"*From:* \n {where}"
+		            }
+		        },
+		        {
+		            "type": "section",
+		            "text": {
+		                "type": "mrkdwn",
+		                "text": f"*Who I am:* \n {you}"
+		            }
+		        },
+		        {
+		            "type": "section",
+		            "text": {
+		                "type": "mrkdwn",
+		                "text": f"*What I want to learn:* \n {learn}"
+		            }
+		        },
+		        {
+		            "type": "section",
+		            "text": {
+		                "type": "mrkdwn",
+		                "text": f"*What I want to work on:* \n {project}"
+		            }
+		        }
+	        ]
+	client.chat_update(channel=data[0],
+                       ts=data[1], blocks=block)
+
+# print shame on you when they do not want to introduce themselves
+@app.action("intro_nope_button")
+def yes_button(body, action, logger, client, ack):
+    ack()
+
+    # creating a new message which has parts of the old messag and adds a section
+    oldMessage = [body['message']['blocks'][0]]
+    oldMessage.append({
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": f"You said nope, SHAME on you!"
+        }
+    })
+
+    # we are updating an existing message here and remove the buttons
+    client.chat_update(channel=body['channel']['id'],
+                       ts=body['message']['ts'], blocks=oldMessage)
+
+@app.message("Test")
+def reply_to_test(say):
+    say("Yes, tests are important!")
 
 # Start app
 if __name__ == "__main__":
